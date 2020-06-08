2480d60f16f65879e613a32fc7b27c3a21548372
morukamu <33851412+morukamu@users.noreply.github.com>
2020-06-08 11:30:46 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>HW 求人のリスト化</title>
+</head>
+<body>
+  <table border="1">
+    <tr>
+      <th colspan="3">株式会社ベストプロジェクト</th>
+    </tr>
+    <tr>
+      <th>受付年月日</th><th>職種</th><th>仕事内容</th>
+    </tr>
+    <tr>
+      <td class="sigoto">2020年5月7日</td><td class="sigoto">(障)一般事務・軽作業</td><td>
+        <ul><li>書類のファイリング、シュレッダー、社内整理、</li>
+          <li>郵便物、荷物仕分け</li>
+          <li>情報のデータ入力、管理</li>
+          <li>資材の準備(梱包・発送・移動)</li>
+        </ul></td>
+    </tr>
+  </table>
+
+  <table border="1">
+    <tr>
+      <th colspan="3">株式会社 アスパーク</th>
+    </tr>
+    <tr>
+      <th>受付年月日</th><th>職種</th><th>仕事内容</th>
+    </tr>
+    <tr>
+      <td class="sigoto">令和2年3月31日</td><td class="sigoto">(障)プログラマー・システムエンジニア</td><td>
+        <ul><li>業務用システムの設計・開発のお仕事</li>
+          <li>WEBアプリに限らず、クライアント・サーバーシステム等の業務システムや汎用機のシステム開発まで幅広い業務</li>
+        </ul></td>
+    </tr>
+  </table>
+
+  <table border="1">
+    <tr>
+      <th colspan="3">株式会社 ブレーンナレッジシステムズ</th>
+    </tr>
+    <tr>
+      <th>受付年月日</th><th>職種</th><th>仕事内容</th>
+    </tr>
+    <tr>
+      <td class="sigoto">令和2年3月17日</td><td class="sigoto">(株) プログラマー</td><td>
+        <ul><li>システム開発における設計実装の中からスキルに合った仕事</li>
+        </ul></td>
+    </tr>
+  </table>
+
+  <table border="1">
+    <tr>
+      <th colspan="3">企業名</th>
+    </tr>
+    <tr>
+      <th>受付年月日</th><th>職種</th><th>仕事内容</th>
+    </tr>
+    <tr>
+      <td class="sigoto">令和2年3月5日</td><td class="sigoto">(障) 技術</td><td>
+        <ul><li>プログラミング、システム設計、開発プロジェクト管理、コンサ
+          ルテーション等でCS(顧客満足度)を最大限に高めるシステム
+          を、提案・構築する。</li>
+        </ul></td>
+    </tr>
+  </table>
+
+  <table border="1">
+    <tr>
+      <th colspan="3">八尾徳洲会総合病院</th>
+    </tr>
+    <tr>
+      <th>受付年月日</th><th>職種</th><th>仕事内容</th>
+    </tr>
+    <tr>
+      <td class="sigoto">令和2年5月13日</td><td class="sigoto">(障) 一般事務</td><td>
+        <ul><li>パソコン入力(データ入力)</li>
+          <li>文書作成</li>
+          <li>診療窓口受付業務等</li>
+        </ul></td>
+    </tr>
+  </table>
+</body>
+</html>
